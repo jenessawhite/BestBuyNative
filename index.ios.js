6fe6f59dfaed28a9ce5a80a7928d72a4217fc17f
Jenessa <jenessaw31@gmail.com>
2016-12-10 18:51:10 -0600

    pushing for error testing

--- index.ios.js
+++ index.ios.js
@@ -4,49 +4,141 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   Text,
+//   ScrollView,
+//   View,
+//   Image,
+//   TouchableHighlight
+// } from 'react-native';
+// import axios from 'axios';
+// import { Match, MemoryRouter as Router } from 'react-router';
+// import api from './Api';
+// import styles from './styles';
+//
+// export class GetProducts extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       products:[]
+//     };
+//   }
+//   componentDidMount() {
+//     this.getProducts()
+//   }
+//   getProducts(){
+//     axios.get(api() + '/products?$limit=25')
+//     .then((response)=> {
+//       console.log(response);
+//       let products = response.data.data.slice(0);
+//       console.log(products);
+//       this.setState({
+//         products
+//       })
+//     })
+//     .catch((error)=> {
+//       console.log(error);
+//     });
+//   }
+//   render() {
+//     return (
+//       <ScrollView style={styles.mainContainer}>
+//         {this.state.products.map((product) =>{
+//           return (
+//             <View key={product.id} style={styles.pContainer}>
+//               <Text style={styles.pName}>
+//                 {product.name}
+//               </Text>
+//               <Text style={styles.pPrice}>
+//                 {product.price}
+//               </Text>
+//               <Text style={styles.pText}>
+//                 {product.manufacturer}: {product.model}
+//               </Text>
+//               <Text style={styles.pText}>
+//                 {product.description}
+//               </Text>
+//             </View>
+//           )
+//         })}
+//       </ScrollView>
+//     );
+//   }
+// }
+//
+// export class GetStores extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       stores:[]
+//     };
+//   }
+//   componentDidMount() {
+//     this.getStores()
+//   }
+//   getStores(){
+//     axios.get(api() + '/stores')
+//     .then((response)=> {
+//       console.log(response);
+//       let stores = response.data.data.slice(0);
+//       console.log(stores);
+//       this.setState({
+//         stores
+//       })
+//     })
+//     .catch((error)=> {
+//       console.log(error);
+//     });
+//   }
+//   render() {
+//     return (
+//       <ScrollView style={styles.mainContainer}>
+//         {this.state.stores.map((store) =>{
+//           return (
+//             <View key={store.id} style={styles.storeContainer}>
+//               <Text style={styles.store}>
+//                 {store.name}
+//               </Text>
+//               <Text style={styles.storeText}>
+//                 {store.address}
+//               </Text>
+//               <Text style={styles.storeText}>
+//                 {store.city}, {store.state} {store.zip}
+//               </Text>
+//             </View>
+//           )
+//         })}
+//       </ScrollView>
+//     );
+//   }
+// }
+//
+// export default class BestBuyNative extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       stores:[],
+//       products: []
+//     };
+//   }
+//   render() {
+//     return (
+//       <Router>
+//         <ScrollView>
+//           <GetStores />
+//           <GetProducts />
+//         </ScrollView>
+//       </Router>
+//     );
+//   }
+// }
+//
+// AppRegistry.registerComponent('BestBuyNative', () => BestBuyNative);
 
-export default class BestBuyNative extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          Is this still working??
-        </Text>
-        <Text style={styles.instructions}>
-          What's hot reloading?
-        </Text>
-      </View>
-    );
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import { AppRegistry } from 'react-native';
+import routes from './app/routes';
 
-AppRegistry.registerComponent('BestBuyNative', () => BestBuyNative);
+AppRegistry.registerComponent('Simple', () => () => routes);
---
+++ styles.js
@@ -0,0 +1,48 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#854124',
+    marginTop: 20,
+    marginBottom: 10
+  },
+  store: {
+    fontFamily: 'GeezaPro-Bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  storeText: {
+    fontFamily: 'Geeza Pro',
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  storeContainer: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    marginBottom: 15
+  },
+  pContainer: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    marginBottom: 15
+  },
+  pName: {
+    fontFamily: 'GeezaPro-Bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  pPrice: {
+    fontFamily: 'Geeza Pro',
+    fontSize: 18,
+    textAlign: 'center',
+    fontStyle: 'italic'
+  },
+  pText: {
+    fontFamily: 'Geeza Pro',
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  }
+});
